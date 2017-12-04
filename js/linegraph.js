4641c4a4cb96311f20caf663f6dae7c95f81a896
Taylor Nodell <tnodell@ssbbartgroup.com>
2017-12-04 13:42:50 -0800

    fixes svg size bug

--- js/linegraph.js
+++ js/linegraph.js
@@ -1,5 +1,7 @@
 function makeLineGraph(json){
-	var thirtyDayStatObj = {};
+	var thirtyDayLineObj = {};
+    var maxPoint = 0;
+    var parseTime = d3.timeParse("%b-%Y-%d-%a");
 
 	/* Format data, each line/category must be an entry in the array */
 	$.each(json, function(date, objectDay){
@@ -14,13 +16,15 @@ function makeLineGraph(json){
     		// Just leave if its the timestamp 
     		if(fishTypeName === "timestamp"){ return;}
     		// Create the empty array if there isn't an array there yet
-    		if(thirtyDayStatObj[fishTypeName] === undefined){ 
-    			thirtyDayStatObj[fishTypeName] = [];
+    		if(thirtyDayLineObj[fishTypeName] === undefined){ 
+    			thirtyDayLineObj[fishTypeName] = [];
     		}
 
     		var avg = objectDay[fishTypeName].avg;
+            // Check the max point for y domain
+            if(avg > maxPoint){ maxPoint = avg;}
     		var item = fishTypeName;
-    		var currentDate = date;
+    		var currentDate = parseTime(date);
     		var stdDev = objectDay[fishTypeName].stdDev;
     		var salesVolume = objectDay[fishTypeName].salesVolume;
 
@@ -32,36 +36,39 @@ function makeLineGraph(json){
 		        "salesVolume": salesVolume
 		     };
 
-			thirtyDayStatObj[fishTypeName].push(lineFishTypeObj);
+			thirtyDayLineObj[fishTypeName].push(lineFishTypeObj);
     	})
       });
-	console.log(thirtyDayStatObj);
+
+
+	console.log(thirtyDayLineObj);
 	var margin = {top: 20, right: 80, bottom: 110, left: 80},
     width = getWidthOfGraph('#lineGraph') - margin.left - margin.right,
     height = 720 - margin.top - margin.bottom;
 
 
-    var svg = d3.select("#lineGraph").append("svg");
-    var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("#lineGraph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
-    // z is color, schemeCategory20 is a 20 color palette
-    var z = d3.scaleOrdinal(d3.schemeCategory20);
+    // schemeCategory20 is a 20 color palette
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
     var line = d3.line()
 	    .curve(d3.curveBasis)
-	    .x(function(d) { 
-            console.log(d)
-            return x(d.date); })
+	    .x(function(d) { return x(d.date); })
 	    .y(function(d) { return y(d.avg); });
 
-	x.domain(d3.extent(thirtyDayStatObj, function(d) { 
-		console.log(d)
-		console.log(d.date)
-		return d.date; }));
+    // Supply the earliest and latest dates
+    //console.log(d3.extent(thirtyDayLineObj.fw, function(d) { return d.date; }))
+	x.domain(d3.extent(thirtyDayLineObj.fw, function(d) { return d.date; }));
 
-  	y.domain([ 0, d3.max(thirtyDayStatObj, function(d) { return d.avg; })])
+    console.log(d3.max(thirtyDayLineObj.fw, function(d) { return d.avg; }))
+  	y.domain([ 0, maxPoint])
 
 
   	// add the x Axis
@@ -79,56 +86,32 @@ function makeLineGraph(json){
  	// add the y Axis
     var yAxis = d3.axisLeft(y);
     svg.append("g")
-        .attr("class", "yAxisStatGraph")
+        .attr("class", "yAxisLineGraph")
         .call(yAxis);
 
-  	svg.append('svg:path')
-	  .attr('d', line(thirtyDayStatObj))
-	  .attr('stroke', 'green')
-	  .attr('stroke-width', 2)
-	  .attr('fill', 'none');
 
-      draw(thirtyDayStatObj, "fw", x, y, svg, line, height)
+    $.each(thirtyDayLineObj, function(type){
+      draw(thirtyDayLineObj, type, x, y, svg, line, height, color)
+    })
+
 }
 
 
-function draw(data, fishTypeName, x, y, svg, line, height) {
+function draw(data, fishTypeName, x, y, svg, line, height, color) {
   
   var data = data[fishTypeName];
-  var parseTime = d3.timeParse("%b-%Y-%d-%a");
-  // format the data
-  data.forEach(function(d) {
-      d.date = parseTime(d.date);
-      d.avg = +d.avg;
-      d.salesVolume = +d.salesVolume;
-  });
   
-  // sort years ascending
-  data.sort(function(a, b){
-    return a["date"]-b["date"];
-    })
  
   // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) {
-      return Math.max(d.avg, d.salesVolume); })]);
-  
-  // Add the line path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", line);
-  // Add the line path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", line);  
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  y.domain([0, d3.max(data, function(d) {return Math.max(d.avg); })]);
 
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
-  }
+  // Add the line path.
+  svg.append("path")
+      .data(data)
+      .attr("class", "line")
+      .style("stroke", function() { // Add the colours dynamically
+                return data.color = color(data.key); })
+      .attr("d", line(data));  
+
+}
